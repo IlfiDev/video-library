605a69107c38ca7c33cf7c4da08259dacb6206d3
IlfiDev <ilyafillipov2@gamil.com>
2022-09-25 23:19:34 +0300

    Added login screen

--- app/src/main/java/com/example/videolibrary/MainActivity.java
+++ app/src/main/java/com/example/videolibrary/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.videolibrary;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.videolibrary.databinding.ActivityMainBinding;
+import com.example.videolibrary.viewmodel.LoginViewModel;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
 
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
